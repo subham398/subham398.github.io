b12992c23d1eecbde88ccf81cce5b5fb49e2e008
Subham Mittal <63242784+subham398@users.noreply.github.com>
2020-08-01 15:16:40 +0530

    Add files via upload

---
+++ test/index.php
@@ -0,0 +1,23 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+<title>Subham Mittal PHP</title>
+</head>
+<body>
+<h1>Subham Mittal PHP </h1>
+<p>This is a para tag</p>
+<?php print hash('sha256', 'Subham Mittal'); ?>
+
+<pre>ASCII ART:
+     ********
+     *
+     *
+     ********
+            *
+            *
+     ********
+
+</pre>
+
+</body>
